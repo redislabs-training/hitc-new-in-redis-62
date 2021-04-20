9d1e5c594679bd077880cb5b74e4c49880bb74cf
Simon Prickett <simon@redislabs.com>
2021-04-19 17:17:52 -0700

    Adds streams, resets data.

--- data/load_all.sh
+++ data/load_all.sh
@@ -2,4 +2,5 @@
 /usr/local/bin/redis-cli < /sampledata/get_alternatives_data.redis
 /usr/local/bin/redis-cli < /sampledata/hrandfield_data.redis
 /usr/local/bin/redis-cli < /sampledata/set_options_data.redis
+/usr/local/bin/redis-cli < /sampledata/streams_minid_data.redis
 /usr/local/bin/redis-cli < /sampledata/smismember_data.redis
