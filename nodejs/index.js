596c5ef41186fd78b2f4824714b8d979586a0cc0
Mephalich <lobach.siarhei@gmail.com>
2022-10-06 08:54:46 -0700

    Update nodejs/index.js
    Co-authored-by: Simon Prickett <simon@crudworks.org>

--- nodejs/index.js
+++ nodejs/index.js
@@ -3,7 +3,7 @@ import { createClient } from 'redis'
 const fnc = async() => {
     // Lets connect
     const client = createClient({ url:'redis://localhost:6379' })
-    client.on('error', (err) => { console.log('Redis Client Error: ', err); process.exit(2)});
+    client.on('error', (err) => { console.log('Redis Client Error: ', err); process.exit(2)})
 
     await client.connect();
 
